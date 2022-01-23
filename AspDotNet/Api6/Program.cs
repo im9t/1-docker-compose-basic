4fdd8547323e85bb57298fe0eda70ab574e8e89c
Hailong.Yan <hailong.yan@thoughtworks.com>
2022-01-23 20:51:23 +0800

    add migration

--- AspDotNet/Api6/Program.cs
+++ AspDotNet/Api6/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TodoContext>(opt =>
-               opt.UseNpgsql("TodoList"));
+               opt.UseNpgsql(builder.Configuration.GetConnectionString("psql")));
 
 var app = builder.Build();
 
