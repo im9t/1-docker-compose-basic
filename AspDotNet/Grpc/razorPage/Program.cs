6664440b669e113354a466fe6343e62a8644cc2f
Hailong.Yan <hailong.yan@thoughtworks.com>
2022-01-27 23:26:59 +0800

    add razorPage

---
+++ AspDotNet/Grpc/razorPage/Program.cs
@@ -0,0 +1,43 @@
+using razorPage.Models;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<TodoContext>(opt =>
+               opt.UseNpgsql(builder.Configuration.GetConnectionString("psql")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+try
+{
+    using var serviceScop = app.Services.CreateScope();
+    var context = serviceScop.ServiceProvider.GetRequiredService<TodoContext>();
+    if(context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+
+}
+catch
+{
+}
+
+app.Run();
--- wordSplit/client4Test.py
+++ wordSplit/client4Test.py
@@ -8,7 +8,7 @@ def run():
     stub = wordSplit_pb2_grpc.WordSplitterStub(channel)
     hellores = stub.SayHello(wordSplit_pb2.HelloRequest(name ="asdfg"))
     print(f"hello returns {hellores.message}")
-    response = stub.GetSplitted(wordSplit_pb2.OriginString(origin = "晴空一鹤排云上"))
+    response = stub.GetSplitted(wordSplit_pb2.OriginString(origin = "我去上学校天天不迟到"))
     print(response.keyWords)
 if __name__ =="__main__":
     print("start")
