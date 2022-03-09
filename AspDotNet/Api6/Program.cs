7fe8bc01d9a59093cfeec5d4d87bd885d7f0796e
Hailong.Yan <hailong.yan@thoughtworks.com>
2022-03-09 22:24:33 +0800

    done

--- AspDotNet/Api6/Program.cs
+++ AspDotNet/Api6/Program.cs
@@ -10,11 +10,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 Console.WriteLine("============Setting connection string============");
-var str = File.ReadAllText("/run/secrets/db_password");
-var password = str.Split("=")[1];
-var connectionString = $"User ID=postgres;Password={password};Host=postgres;Port=5432;Database=myDataBase;";
-builder.Services.AddDbContext<TodoContext>(opt =>
-               opt.UseNpgsql(connectionString));
+try
+{
+    bool folderExists = Directory.Exists("/run/secrets/");
+    Console.WriteLine($"Folder secrets exsits: {folderExists}");
+    var files =  Directory.GetFiles("/run/secrets/");
+    foreach(var file in files)
+    {
+        Console.WriteLine($"Folder secrets exsits: {file}");
+    }
+    var password = File.ReadAllText("/run/secrets/db_password");
+    var connectionString = $"User ID=postgres;Password={password};Host=postgres;Port=5432;Database=myDataBase;";
+    builder.Services.AddDbContext<TodoContext>(opt =>
+                   opt.UseNpgsql(connectionString));
+
+}
+catch
+{
+    Console.WriteLine("============Exceptioned============");
+    var connectionString = $"User ID=postgres;Password=abc123;Host=postgres;Port=5432;Database=myDataBase;";
+    builder.Services.AddDbContext<TodoContext>(opt =>
+                   opt.UseNpgsql(connectionString));
+
+}
 Console.WriteLine("============Setting connection string done============");
 builder.Services.AddCors(options =>
 {
