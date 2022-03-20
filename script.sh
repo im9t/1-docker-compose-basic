dc0637a852f74d68bcec410a4e5d23301644214a
Hailong.Yan <hailong.yan@thoughtworks.com>
2022-03-20 20:15:51 +0800

    script

---
+++ script.sh
@@ -0,0 +1,16 @@
+while [ -n "$1" ]  
+do  
+  case "$1" in   
+    -build)  
+        docker build  -t wasm ./AspDotNet/BlazorWasm/
+        ;;  
+    -up)  
+        docker service update --force  --image wasm:latest  devops_dotnet
+        shift  
+        ;;  
+    *)  
+        echo "$1 is not an option"  
+        ;;  
+  esac  
+  shift  
+done
